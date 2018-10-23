ef37571de3f192e41bb85fce57fb28a253b79e71
Juhi1919 <juhipatel9394@gmail.com>
2018-10-23 18:18:08 -0400

    changes in testing filesj

--- test/test.js
+++ test/test.js
@@ -2,14 +2,20 @@ var Nightmare = require("nightmare");
 
 new Nightmare({ show: true })
   // Visit login page
-  .goto("https://localhost:8087/")
+  .goto("https://mighty-hamlet-89198.herokuapp.com/")
   // Enter user email.
   .type("#inlineFormInputGroupUsername2", "juhi@gmail.com")
   // Enter password.
   .type("#inlineFormInputGroupPassword2", "juhi@1919")
   // Click login button.
   .click("#btnLogin")
-  
+  //go to member page
+  //.goto("https://mighty-hamlet-89198.herokuapp.com/members")
+  //add post
+  .type("#title","Favourite Game")
+  .type("#formbody","Favourite Game is Mario")
+  // submit post
+  .click("#newblogsubmitbtn")
   // End test
   .end()
   // Execute commands
